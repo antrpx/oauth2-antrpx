17a40e288d343e8001929bf404fa5772407c0707
Maxim Sablev <maxim.sablev@gmail.com>
2018-06-11 21:50:49 +0300

    * initial project sources

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# oauth2-antrpx
+# Antrpx Provider for OAuth 2.0 Client
+[![Software License](https://img.shields.io/badge/license-MIT-brightgreen.svg?style=flat-square)](LICENSE.md)
+
+This package provides Antrpx (https://www.antrpx.io/) OAuth 2.0 support for the PHP League's [OAuth 2.0 Client](https://github.com/thephpleague/oauth2-client).
+
+## Installation
+
+To install, use composer:
+
+```
+composer require antrpx/oauth2-antrpx
+```
+
+## Credits
+
+- [Maxim Sablev](https://github.com/sablev)
+- [All Contributors](https://github.com/antrpx/oauth2-antrpx/contributors)
+
+
+## License
+
+The MIT License (MIT). Please see [License File](https://github.com/mugnate/oauth2-ecwid/blob/master/LICENSE) for more information.
---
+++ src/Provider/Antrpx.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace Antrpx\OAuth2\Client\Provider;
+
+use League\OAuth2\Client\Provider\AbstractProvider;
+use League\OAuth2\Client\Provider\Exception\IdentityProviderException;
+use League\OAuth2\Client\Token\AccessToken;
+use League\OAuth2\Client\Tool\BearerAuthorizationTrait;
+use Psr\Http\Message\ResponseInterface;
+
+class Antrpx extends AbstractProvider
+{
+    use BearerAuthorizationTrait;
+
+    /**
+     * Antrpx resource server URL.
+     *
+     * @const string
+     */
+    const BASE_ANTRPX_RESOURCE_SERVER_URL = 'https://api.antrpx.io/v1';
+
+    /**
+     * Antrpx authorization server URL.
+     *
+     * @const string
+     */
+    const BASE_ANTRPX_AUTH_SERVER_URL = 'https://api.antrpx.io/o';
+
+    /**
+     * @param array $options
+     * @param array $collaborators
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function __construct(array $options = [], array $collaborators = [])
+    {
+        if (empty($options['clientId'])) {
+            throw new \InvalidArgumentException('The "clientId" option not set. Please set it.');
+        } elseif (empty($options['clientSecret'])) {
+            throw new \InvalidArgumentException('The "clientSecret" option not set. Please set it.');
+        } elseif (empty($options['redirectUri'])) {
+            throw new \InvalidArgumentException('The "redirectUri" option not set. Please set it.');
+        }
+
+        parent::__construct($options, $collaborators);
+    }
+
+    public function getBaseAuthorizationUrl()
+    {
+        return static::BASE_ANTRPX_AUTH_SERVER_URL.'/authorize';
+    }
+
+    public function getBaseAccessTokenUrl(array $params)
+    {
+        return static::BASE_ANTRPX_AUTH_SERVER_URL.'/token';
+    }
+
+    public function getResourceOwnerDetailsUrl(AccessToken $token)
+    {
+        return static::BASE_ANTRPX_RESOURCE_SERVER_URL.'/user';
+    }
+
+    protected function getDefaultScopes()
+    {
+        return [
+            'accounts:item:read',
+            'profiles:list:read',
+            'profiles:item:read',
+            'metrics:list:read',
+            'metrics:item:read'
+        ];
+    }
+
+    protected function checkResponse(ResponseInterface $response, $data)
+    {
+        if (isset($data['error']) || $response->getStatusCode() != 200) {
+            throw new IdentityProviderException($data['error'], $response->getStatusCode(), $response);
+        }
+    }
+
+    protected function createResourceOwner(array $response, AccessToken $token)
+    {
+        return new AntrpxUser($response);
+    }
+}
